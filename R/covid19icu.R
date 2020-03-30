c7094fa588cec363c2655c0819d4493d00f40c69
Soheil Eshghi <soheil.eshghi@gmail.com>
2020-03-30 09:22:35 -0400

    sourcing yaml + 1st attempt at yaml

--- R/covid19icu.R
+++ R/covid19icu.R
@@ -1,3 +1,3 @@
 #' COVID19 ICU Dynamics
-#' @import shiny dplyr ggplot2 shinythemes cowplot deSolve tidyverse tidyr
+#' @import shiny dplyr ggplot2 shinythemes cowplot deSolve tidyverse tidyr yaml
 "_PACKAGE"
