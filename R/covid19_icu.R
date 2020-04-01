882926f85d79908da23ca6e6d25fc84fffa33f61
mcerlen <mcerlen@users.noreply.github.com>
2020-04-01 09:00:31 -0400

    CFR and remove data.table and xtable

--- R/covid19_icu.R
+++ R/covid19_icu.R
@@ -1,3 +1,3 @@
 #' COVID19 ICU Dynamics
-#' @import shiny dplyr ggplot2 shinythemes cowplot deSolve tidyverse tidyr yaml data.table xtable
+#' @import shiny dplyr ggplot2 shinythemes cowplot deSolve tidyverse tidyr yaml
 "_PACKAGE"
