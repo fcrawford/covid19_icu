404a8762868d0e1997c90fc491f05b19c44ff8c9
Soheil Eshghi <soheil.eshghi@gmail.com>
2020-04-15 11:19:31 -0400

    fix text output

--- R/app_textoutputs.R
+++ R/app_textoutputs.R
@@ -6,7 +6,7 @@ text_hospital = function(doprotocols=0, dynamicModel=0, params, ...){
   
     hospital_input <- hospital_input_generation (dynamicModel=dynamicModel, params=params)
     
-    if (dynamicModel==1){
+    if (dynamicModel!=0){
       params$t= length(hospital_input)
     }
     
